e3e51f5406fadb06495752d112b732ee351a2fe7
Tina <tina@Tinas-MacBook-Pro.local>
2018-10-08 15:31:17 +0200

    Add custom currency requests

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        reqc: {
+            demand: true,
+            alias: 'reqcurrency',
+            describe: 'Request currency',
+            string: true
+        },
+        resc: {
+            demand: true,
+            alias: 'rescurrency',
+            describe: 'Response currency',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv
+
+let encodedReqCurrency = encodeURIComponent(argv.reqc);
+let encodedResCurrency = encodeURIComponent(argv.resc);
+
+// console.log(argv);
 
 request({
-    url: 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=RSD',
+    url: `https://min-api.cryptocompare.com/data/price?fsym=${encodedReqCurrency}&tsyms=${encodedResCurrency}`,
     json: true
 }, (error, response, body) => {
 
-    console.log(`The price of the Bitcoin is ${body.RSD} RSD`);
+    console.log(body);
 });
 
 //statusCode
