7ee24d24cd5436dbd6b682012a4dff10fcb5620b
Tina <tina@Tinas-MacBook-Pro.local>
2018-10-10 10:53:27 +0200

    Add list and RSD value request

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
 const request = require('request');
 const yargs = require('yargs');
 
-const argv = yargs
-    .options({
-        reqc: {
-            demand: true,
-            alias: 'reqcurrency',
-            describe: 'Request currency',
-            string: true
-        },
-        resc: {
-            demand: true,
-            alias: 'rescurrency',
-            describe: 'Response currency',
-            string: true
-        },
+const reqc = {
+    demand: true,
+    alias: 'reqcurrency',
+    describe: 'Request currency',
+    string: true
+}
 
+const resc = {
+    demand: true,
+    alias: 'rescurrency',
+    describe: 'Response currency',
+    string: true
+}
+
+const argv = yargs
+    .command('value', 'Find value of selected currency', {
+        request: reqc,
+        response: resc
+    })
+    .command('list', 'Request coin list')
+    .command('rsd', 'Fetch RSD value for selected currency', {
+        request: resc
     })
     .help()
     .alias('help', 'h')
     .argv
 
-let reqCurrency = (argv.reqc).replace(/\s*,\s*/g, ",");
-let resCurrency = (argv.resc).replace(/\s*,\s*/g, ",");
+let command = argv._[0]
 
-request({
-    url: `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${reqCurrency}&tsyms=${resCurrency}`,
-    json: true
-}, (error, response, body) => {
-    console.log(body);
-});
+if (command === 'list') {
+    request({
+        url: 'https://min-api.cryptocompare.com/data/all/coinlist',
+        json: true
+    }, (error, response, body) => {
+        console.log(body);
+    })
+} else if (command === 'value') {
+    let reqCurrency = (argv.request).replace(/\s*,\s*/g, ",");
+    let resCurrency = (argv.response).replace(/\s*,\s*/g, ",");
+    request({
+        url: `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${reqCurrency}&tsyms=${resCurrency}`,
+        json: true
+    }, (error, response, body) => {
+        console.log(body);
+    });
+} else if (command === 'rsd') {
+    let reqCurrency = (argv.request).replace(/\s*,\s*/g, ",");
+    request({
+        url: `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${reqCurrency}&tsyms=RSD`,
+        json: true
+    }, (error, response, body) => {
+        console.log(body);
+    });
+}
